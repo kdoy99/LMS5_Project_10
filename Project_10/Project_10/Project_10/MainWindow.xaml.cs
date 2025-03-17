11af0d7e550cc4e31f6342981032d1669aa3e2e9
kdy <kdoy990805@gmail.com>
2025-03-17 11:44:45 +0900

    웹캠 WPF에 연결, 패키지 추가

--- Project_10/Project_10/Project_10/MainWindow.xaml.cs
+++ Project_10/Project_10/Project_10/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
+using System.Drawing;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Data;
 
 namespace Project_10
 {
@@ -16,9 +22,68 @@ namespace Project_10
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count == 0)
+            {
+                MessageBox.Show("비디오 장치를 찾을 수 없습니다.");
+                return;
+            }
+
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            videoSource.Start();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.NewFrame -= new NewFrameEventHandler(video_NewFrame);
+                videoSource = null;
+            }
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            BitmapImage bi;
+            using (var bitmap = (Bitmap)eventArgs.Frame.Clone())
+            {
+                bi = BitmapToBitmapImage(bitmap);
+            }
+
+            bi.Freeze();
+            // 비동기
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                webcamImage.Source = bi;
+            }));
+        }
+
+        private BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new System.IO.MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
         }
     }
 }
